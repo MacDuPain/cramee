fd0978f2abbff380e1676b28eb7292fdff3cc1c5
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-06-04 11:44:29 +0200

    Added Carts

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
+  has_many :cart_items
+  has_many :carts, through: :cart_items
 end
