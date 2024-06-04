4408e17e4bfab3fa7be1e63c746e7198b15e251d
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-06-04 11:47:09 +0200

    Added validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, presence: true
-  has_many :cart_items
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
 end
