ff2232b82535e7c70882adf433deb602974eb68f
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-06-04 16:55:22 +0200

    Added Orders & Shipping info in the orders view. Minor changes to Navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_04_142423) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_04_143551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_142423) do
   create_table "order_items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "order_id", null: false
+    t.bigint "item_id", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -66,5 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_142423) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "items"
   add_foreign_key "carts", "users"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
 end
