5d91aa52c41f00f9c142b09c4e053cbe2336f50a
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-06-10 14:49:29 +0200

    Changed Images sizes and added some meta data

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  has_many :item_taggings
+  has_many :item_taggings, dependent: :destroy
   has_many :item_tags, through: :item_taggings
   has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
