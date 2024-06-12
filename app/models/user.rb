88a0f9838459607095048a552b5a30ce9fce9eaf
MacDuPain <godel.Sacha@gmail.com>
2024-06-12 11:04:10 +0200

    Validation du format mail pour l'inscription

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validate :password_complexity
+  validates :email, format: { with: /\A[\w+\-.]+@(gmail\.com|outlook\.com|yahoo\.com|aol\.com|icloud\.com|protonmail\.com|zoho\.com|yandex\.com|mail\.com|gmx\.com|outlook\.fr|yahoo\.fr|aol\.fr|icloud\.fr|protonmail\.fr|zoho\.fr|yandex\.fr|mail\.fr|gmx\.fr)\z/i, message: "doit être une adresse email valide de services populaires" }
 
   private
 
@@ -32,4 +33,3 @@ class User < ApplicationRecord
     end
   end
 end
-
