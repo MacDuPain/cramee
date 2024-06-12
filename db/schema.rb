97f3e33609a6cb67e4da7cf425773105e8380e34
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-06-12 14:21:53 +0200

    Added Feature Visible Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_12_093436) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_12_115807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_12_093436) do
     t.datetime "updated_at", null: false
     t.integer "quantity"
     t.boolean "in_stock"
+    t.boolean "visible_on_site"
   end
 
   create_table "items_tags", id: false, force: :cascade do |t|
