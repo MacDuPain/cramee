571d2b026eefe23dde19e8d8e15f1b171aa56c20
MacDuPain <godel.Sacha@gmail.com>
2024-06-12 15:12:30 +0200

    Correction du formulaire de commentaire

--- app/javascript/comment_form_validation.js
+++ app/javascript/comment_form_validation.js
@@ -1,17 +1,15 @@
-// app/assets/javascripts/comment_form_validation.js
 document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("comment-form");
-  
-    form.addEventListener("submit", function(event) {
-      const contentField = document.getElementById("comment-content");
-      
-      if (!contentField.value.trim()) {
-        alert("Le commentaire ne peut pas être vide.");
-        contentField.classList.add("is-invalid");
-        event.preventDefault();
-      } else {
-        contentField.classList.remove("is-invalid");
-      }
-    });
+  const form = document.querySelector(".custom-form");
+
+  form.addEventListener("submit", function(event) {
+    const contentField = form.querySelector("input[name='comment[content]']");
+
+    if (!contentField.value.trim()) {
+      alert("Veuillez entrer un commentaire.");
+      contentField.classList.add("is-invalid");
+      event.preventDefault();
+    } else {
+      contentField.classList.remove("is-invalid");
+    }
   });
-  
+});
