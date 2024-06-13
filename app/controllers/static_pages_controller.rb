437616bc946a44f21eded8a343d0557706ef1b5f
MacDuPain <164226646+MacDuPain@users.noreply.github.com>
2024-06-13 15:02:28 +0200

    Revert ""mentions légales""

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,4 @@ class StaticPagesController < ApplicationController
   def landing_page
     @items = Item.all
   end
-
-  def mentions_legales
-    # This action is empty because we will render a static page directly
-  end
 end
