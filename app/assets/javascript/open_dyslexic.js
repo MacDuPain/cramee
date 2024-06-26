4140bd0f614c9053d7285d1127fdaf447227451f
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-06-26 12:35:34 +0200

    Moved Dyslexic btn and changed into an img

--- app/assets/javascript/open_dyslexic.js
+++ app/assets/javascript/open_dyslexic.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const toggleButton = document.getElementById('toggleFont');
-
-  if (toggleButton) {
-      toggleButton.addEventListener('click', function() {
-          // Liste des éléments à cibler
-          const elementsToToggle = document.querySelectorAll('body, h1, h2, h3, h4, h5, h6, p, span, div');
-
-          elementsToToggle.forEach(function(element) {
-              element.classList.toggle('dyslexia-font');
-          });
-
-          if (document.body.classList.contains('dyslexia-font')) {
-              toggleButton.textContent = 'Police pour dyslexie Désactivée';
-          } else {
-              toggleButton.textContent = 'Police pour dyslexie Activée';
-          }
-      });
-  } else {
-      console.error('Toggle button not found');
-  }
-});
+    const toggleLink = document.getElementById('toggleFont');
+  
+    if (toggleLink) {
+        toggleLink.addEventListener('click', function(event) {
+            event.preventDefault(); // Empêche le comportement par défaut du lien
+  
+            // Liste des éléments à cibler
+            const elementsToToggle = document.querySelectorAll('body, h1, h2, h3, h4, h5, h6, p, span, div');
+  
+            elementsToToggle.forEach(function(element) {
+                element.classList.toggle('dyslexia-font');
+            });
+  
+            if (document.body.classList.contains('dyslexia-font')) {
+                toggleLink.title = 'Police pour dyslexie Désactivée';
+            } else {
+                toggleLink.title = 'Police pour dyslexie Activée';
+            }
+        });
+    } else {
+        console.error('Toggle link not found');
+    }
+  });
+  
