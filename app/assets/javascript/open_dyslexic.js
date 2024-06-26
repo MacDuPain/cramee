8166c1f1ad0f411c5ce75f0cb4d28de2b261d6c8
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-06-26 14:57:47 +0200

    Changed the button and placed it on the header

--- app/assets/javascript/open_dyslexic.js
+++ app/assets/javascript/open_dyslexic.js
@@ -1,25 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleLink = document.getElementById('toggleFont');
   
+    const isDyslexiaFontEnabled = localStorage.getItem('dyslexiaFontEnabled') === 'true';
+    if (isDyslexiaFontEnabled) {
+        toggleDyslexiaFont(true);
+    }
+  
     if (toggleLink) {
         toggleLink.addEventListener('click', function(event) {
             event.preventDefault(); // Empêche le comportement par défaut du lien
-  
-            // Liste des éléments à cibler
-            const elementsToToggle = document.querySelectorAll('body, h1, h2, h3, h4, h5, h6, p, span, div');
-  
-            elementsToToggle.forEach(function(element) {
-                element.classList.toggle('dyslexia-font');
-            });
-  
-            if (document.body.classList.contains('dyslexia-font')) {
-                toggleLink.title = 'Police pour dyslexie Désactivée';
-            } else {
-                toggleLink.title = 'Police pour dyslexie Activée';
-            }
+            const isEnabled = document.body.classList.contains('dyslexia-font');
+            toggleDyslexiaFont(!isEnabled);
         });
     } else {
         console.error('Toggle link not found');
     }
-  });
+
+    function toggleDyslexiaFont(enable) {
+        const elementsToToggle = document.querySelectorAll('body, h1, h2, h3, h4, h5, h6, p, span, div');
   
+        elementsToToggle.forEach(function(element) {
+            if (enable) {
+                element.classList.add('dyslexia-font');
+            } else {
+                element.classList.remove('dyslexia-font');
+            }
+        });
+  
+        if (enable) {
+            toggleLink.title = 'Police pour dyslexie Désactivée';
+            localStorage.setItem('dyslexiaFontEnabled', 'true');
+        } else {
+            toggleLink.title = 'Police pour dyslexie Activée';
+            localStorage.setItem('dyslexiaFontEnabled', 'false');
+        }
+    }
+});
